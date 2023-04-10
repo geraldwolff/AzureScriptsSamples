2e0cc5aded4b636995d034ec4bc47aeda0a07eb2
jerrywolff <gerald.wolff@outlook.com>
2023-04-09 20:31:01 -0700

    Azure workbook extract and query tool
    Powershell script to locate, extract serialized data, pull queries and allow querires to be executed for results that are presented in html and csv

---
+++ workbook_json_extract/Extract_queries_from_migration_workbook_and_generate_audit_ogv_localfile.ps1
@@ -0,0 +1,208 @@
+﻿ <#
+.SYNOPSIS  
+   script for Azure deplyment work book Audit tool
+.DESCRIPTION  
+   script for Azure deplyment work book Audit tool
+.EXAMPLE  
+
+    Extract_queries_from_migration_workbook_and_generate_audit_ogv.ps1e_blob_storage_inventory.ps1
+    Intercative script to read all Deployment workbook Queries to audit the deployed resources
+    Uses Az-Resourcegraph and Json 
+Version History  
+v1.0   - Initial Release  
+ 
+
+.NOTES
+
+    THIS CODE-SAMPLE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED 
+
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR 
+
+    FITNESS FOR A PARTICULAR PURPOSE.
+
+    This sample is not supported under any Microsoft standard support program or service. 
+
+    The script is provided AS IS without warranty of any kind. Microsoft further disclaims all
+
+    implied warranties including, without limitation, any implied warranties of merchantability
+
+    or of fitness for a particular purpose. The entire risk arising out of the use or performance
+
+    of the sample and documentation remains with you. In no event shall Microsoft, its authors,
+
+    or anyone else involved in the creation, production, or delivery of the script be liable for 
+
+    any damages whatsoever (including, without limitation, damages for loss of business profits, 
+
+    business interruption, loss of business information, or other pecuniary loss) arising out of 
+
+    the use of or inability to use the sample or documentation, even if Microsoft has been advised 
+
+    of the possibility of such damages, rising out of the use of or inability to use the sample script, 
+
+    even if Microsoft has been advised of the possibility of such damages.
+
+#> 
+
+
+ $querylist = ''
+ $resultcollection = ''
+
+ $workbookpath  = "C:\temp\"
+ $workbook = "subscription_workbook.json"
+###########################
+
+# Read in the Azure Workbook template
+$json = Get-Content -Path "$($workbookpath)$($workbook)"  -Raw | ConvertFrom-Json
+
+# Extract the query from the workbook parameters
+#$parameters = $json.items | Where-Object { $_.type -eq 9 } | Select-Object -ExpandProperty content
+ 
+
+# Extract the subscription IDs from the workbook source data
+#$sourceData = $json.items | Where-Object { $_.type -eq 11 } | Select-Object -ExpandProperty content #.links
+
+$sourcequeries = $json.items  | select-object -ExpandProperty content
+$queries = $sourcequeries.items.content.query 
+
+$links = $($sourceData).links 
+
+  
+    Foreach($jsonitem in ($sourcequeries | where-object { $_.items.content.query  -ne $null} ))
+    {
+
+         #   Write-host "$($jsonitem.items.content.query)" -ForegroundColor Cyan
+
+
+
+        foreach($jsonitemcontent in ($($jsonitem.items.content) | where query -ne $null )  )
+        {
+
+            $contentprops = $($jsonitem.items.content)  
+    
+            $contenttypes = $($jsonitem.items.type)
+
+            foreach($contenttype in $contenttypes)
+            {
+ 
+                $jsoncontentobj = new-object PSObject 
+
+               $contentitems =  $($jsonitem.items.content)  
+               $Feature =  $($contentitems.json) -replace('{#','')
+     
+                $jsoncontentobj | Add-Member -MemberType NoteProperty -Name Type -value $($contenttype)
+                $jsoncontentobj | Add-Member -MemberType NoteProperty -Name Feature -value "$Feature"
+                $jsoncontentobj | Add-Member -MemberType NoteProperty -Name Version -value $($jsonitemcontent.version)  
+                $jsoncontentobj | Add-Member -MemberType NoteProperty -Name title -value $($jsonitemcontent.title)      
+                $jsoncontentobj | Add-Member -MemberType NoteProperty -Name noDataMessage -value $($jsonitemcontent.noDataMessage)
+                $jsoncontentobj | Add-Member -MemberType NoteProperty -Name queryType -value $($jsonitemcontent.queryType)  
+                $jsoncontentobj | Add-Member -MemberType NoteProperty -Name resourceType -value $($jsonitemcontent.resourceType)      
+                $jsoncontentobj | Add-Member -MemberType NoteProperty -Name crossComponentResources -value $($jsonitemcontent.crossComponentResources)
+                $jsoncontentobj | Add-Member -MemberType NoteProperty -Name query -value "$($jsonitemcontent.query)"  
+           
+ 
+        
+               # $jsoncontentobj
+                [array]$querylist +=    $jsoncontentobj
+               
+
+
+            } 
+    }
+ 
+ }
+
+ $selectqueries = $querylist|   Select -unique title,queryType ,resourceType ,query  | ogv -Title " Select one or more qureies to get results" -PassThru | Select -unique title ,query 
+
+
+ foreach($queryselected in $selectqueries)
+ {
+
+ 
+       $resultcollection = ''
+       $queryresults =  Search-AzGraph -Query $($queryselected.query) 
+       write-host "$($queryselected.title) running " -ForegroundColor Cyan
+
+        $queryresults | export-csv c:\temp\$($queryselected.title)_results.csv -NoTypeInformation
+} 
+
+
+$ResultsCSS = @"
+<Title>$($queryselected.title) -  Query ReportReport:$(Get-Date -Format 'dd MMMM yyyy' )</Title>
+<Style>
+th {
+	font: bold 11px "Trebuchet MS", Verdana, Arial, Helvetica,
+	sans-serif;
+	color: #FFFFFF;
+	border-right: 1px solid #C1DAD7;
+	border-bottom: 1px solid #C1DAD7;
+	border-top: 1px solid #C1DAD7;
+	letter-spacing: 2px;
+	text-transform: uppercase;
+	text-align: left;
+	padding: 6px 6px 6px 12px;
+	background: #5F9EA0;
+}
+td {
+	font: 11px "Trebuchet MS", Verdana, Arial, Helvetica,
+	sans-serif;
+	border-right: 1px solid #C1DAD7;
+	border-bottom: 1px solid #C1DAD7;
+	background: #fff;
+	padding: 6px 6px 6px 12px;
+	color: #6D929B;
+}
+</Style>
+"@
+
+
+ 
+ #############################################################
+ 
+ 
+     $CSS = @"
+<Title>$workbook Query ReportReport:$(Get-Date -Format 'dd MMMM yyyy' )</Title>
+<Style>
+th {
+	font: bold 11px "Trebuchet MS", Verdana, Arial, Helvetica,
+	sans-serif;
+	color: #FFFFFF;
+	border-right: 1px solid #C1DAD7;
+	border-bottom: 1px solid #C1DAD7;
+	border-top: 1px solid #C1DAD7;
+	letter-spacing: 2px;
+	text-transform: uppercase;
+	text-align: left;
+	padding: 6px 6px 6px 12px;
+	background: #5F9EA0;
+}
+td {
+	font: 11px "Trebuchet MS", Verdana, Arial, Helvetica,
+	sans-serif;
+	border-right: 1px solid #C1DAD7;
+	border-bottom: 1px solid #C1DAD7;
+	background: #fff;
+	padding: 6px 6px 6px 12px;
+	color: #6D929B;
+}
+</Style>
+"@
+
+
+
+ 
+
+(($querylist | Select -unique    Version,title, noDataMessage,queryType , resourceType, crossComponentResources,query  |`
+  ConvertTo-Html -Head $CSS ).replace('Â Â','')) | out-file  "c:\temp\$($workbook)Queries.html"
+#invoke-item  "c:\temp\$($workbook)Queries.html"
+ 
+ $querylist | Select -unique   Version,title, noDataMessage,queryType , resourceType, crossComponentResources,query |  `
+ export-csv "C:\temp\$($workbook)Queries.csv" -NoTypeInformation
+ 
+
+
+
+
+
+
+
